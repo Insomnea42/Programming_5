ec74375c24efcd1a15a27e40a6d8069360078182
insomnea42 <khveroe@gmail.com>
2022-08-26 16:41:54 +0500

    Непонятненькие проблемы с гитигнором

---
+++ HW_2/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+
+int[] arr = new int[8];
+int sum = 0, leng = arr.Length;
+Console.WriteLine("Массив случайных чисел: ");
+for (int i = 0; i < leng; i++)
+{
+    arr[i] = new Random().Next(-42, 43);
+    Console.WriteLine(i + "-й элемент: " + arr[i] + "; ");
+    if (i % 2 != 0) sum = sum + arr[i];
+}
+Console.WriteLine("Сумма элементов на нечётных позициях: " + sum);
---
+++ HW_3/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+
+double[] arr = new double[6]; // вещественные
+double sum = 0;
+double a;
+int leng = arr.Length;
+Console.WriteLine("Массив случайных чисел: ");
+for (int i = 0; i < leng; i++)
+{
+    a = new Random().Next(0, 43);
+    a = a / 0.423; //преобразование в вещественные
+    arr[i] = a;
+    Console.WriteLine(i + "-й элемент: " + arr[i] + "; ");
+}
+
+double max = arr[0], min = arr[0];
+
+for (int i = 1; i < leng; i++)
+{
+    if (max <= arr[i]) max = arr[i];
+    else
+    {
+        if (min >= arr[i]) min = arr[i];
+    }
+}
+Console.WriteLine("Максимум: " + max + ". Минимум: " + min);
+sum = max - min;
+Console.WriteLine("Разница: " + sum);
